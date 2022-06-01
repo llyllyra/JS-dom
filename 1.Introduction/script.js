cf784b248e780a61b00a7f858855507f1e188025
llyllyra <llyllyrallylly@gmail.com>
2022-06-01 16:21:57 +0200

    exercice 1

--- 1.Introduction/script.js
+++ 1.Introduction/script.js
@@ -1,2 +1,23 @@
 /*
 */
+const body = document.querySelector("body");
+let docTitle = document.title
+console.log(docTitle);
+document.querySelector("title").innerHTML="Modifying the DOM";
+
+for (let children of body.children){
+    console.log(children);
+}
+
+let randomBGC = () =>{
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    const color = `rgb(${r},${g},${b})`;
+    document.body.style.background = color;
+}
+
+setInterval(randomBGC, 2000)
+
+
--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,2 +1,13 @@
 /*
 */
+
+document.querySelector(".important").setAttribute("title", "important")
+
+let img = document.querySelectorAll("img"), i;
+let imgImportant = document.querySelector(".important")
+
+for (i = 0; i < img.length; ++i) {
+   if (imgImportant){
+       img.setAttribute("display", "none")
+   }
+}
