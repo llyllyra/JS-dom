cacea83a175f1b38506de67e8e6ca3915dd5fd2b
llyllyra <llyllyrallylly@gmail.com>
2022-06-02 10:41:13 +0200

    exercice 4

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -1,2 +1,22 @@
 /*
 */
+const list = document.querySelector("ol");
+const firstChild = list.firstChild;
+const lastChild= list.children[4];
+list.insertBefore(lastChild, firstChild);
+
+let main = document.querySelector("main");
+let secondSection = main.children[0].nextElementSibling
+let thirdSection = secondSection.nextElementSibling
+
+let titleSecondSection = secondSection.querySelector("h2")
+let titleThirdSection = thirdSection.querySelector("h2")
+
+let p2 = secondSection.querySelector("p")
+let p3 = thirdSection.querySelector("div")
+
+secondSection.insertBefore(secondSection.appendChild(titleThirdSection), p2)
+
+thirdSection.insertBefore(thirdSection.appendChild(titleSecondSection), p3)
+
+thirdSection.remove()
