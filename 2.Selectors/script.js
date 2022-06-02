b653f45b43014b1e7782fe890c5184b83d35f98a
llyllyra <llyllyrallylly@gmail.com>
2022-06-02 09:54:55 +0200

    exercice 2 et 3

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,13 +1,37 @@
 /*
 */
 
-document.querySelector(".important").setAttribute("title", "important")
+let importants = document.querySelectorAll(".important");
+let important = document.querySelector(".important");
+
+for (important of importants){
+    important.setAttribute("title", "This is an important item")
+}
 
 let img = document.querySelectorAll("img"), i;
-let imgImportant = document.querySelector(".important")
+let imgImportant = document.querySelector("img.important")
 
-for (i = 0; i < img.length; ++i) {
+for (img of img) {
+    img.style.display = "none"
    if (imgImportant){
-       img.setAttribute("display", "none")
+       imgImportant.style.display = "block"
    }
 }
+
+const image = document.querySelectorAll("img");
+image.forEach((item) => {
+    if (item.classList != "important") {
+        item.style.display = "none";
+    }
+});
+//
+const paragraphs = document.querySelectorAll("p");
+paragraphs.forEach(function (item) {
+    // console.log(item.innerHTML);
+    // console.log(item.classList);
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const colorRandom = `rgb(${r},${g},${b})`;
+    if (item.classList != "important") item.style.color = colorRandom;
+});
--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,2 +1,68 @@
 /*
 */
+
+let learner = [
+    "Melih",
+    "Dylan",
+    "Semih",
+    "Marlène",
+    "Louis",
+    "Olivier",
+    "Henri",
+    "Mehdi",
+    "Lydia",
+    "Steeve",
+    "Tanguy",
+    "Anaïs",
+    "Mariya",
+    "Nicolas",
+    "Latifa",
+    "Arthur",
+    "Duc",
+    "Dorian",
+    "Quentin",
+    "Antoine",
+    "Miguel",
+    "Kevin",
+    "Sébastien"
+];
+
+const randomGenerator = (n) => {
+    return Math.floor(Math.random() * n)
+}
+function randomized(learner) {
+    let number = learner.length, t
+    const newLearner = learner
+    while (number != 0) {
+        let randomIndex = randomGenerator(number--);
+        t = newLearner[number];
+        newLearner[number] = newLearner[randomIndex]
+        newLearner[randomIndex] = t
+
+    }
+    return newLearner
+}
+
+
+let addSection = () => {
+    let random = randomized(learner), i
+    let newSection = document.createElement("section");
+    const article = document.querySelector("article")
+    for (i = 0; i < random.length; i++) {
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255); 
+        const color = `rgb(${r},${g},${b})`;
+
+        article.appendChild(newSection, article);
+        let paragraphe = document.createElement("p");
+        let peopleSection = document.createTextNode(random[i]);
+        newSection.appendChild(paragraphe);
+        paragraphe.appendChild(peopleSection);
+        paragraphe.style.background = color;
+        r < 150 && g < 150
+            ?(paragraphe.style.color = "white")
+            : (paragraphe.style.color = "black");
+    }
+}
+addSection()
