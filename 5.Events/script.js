8bb650574536248fde11f9ffaa8701f6ed239210
llyllyra <llyllyrallylly@gmail.com>
2022-06-03 10:54:27 +0200

    exercice 6

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -73,3 +73,4 @@ const saycolor = (e)=>{
 }
 let squares = document.createElement("div.displayedsquare")
 displayedquare.addEventListener("click", saycolor)
+
--- 6.Forms/script.js
+++ 6.Forms/script.js
@@ -1,2 +1,52 @@
 /*
 */
+let inputNames = document.getElementById("firstname");
+let spanFirstName = document.getElementById("display-firstname");
+
+inputNames.addEventListener("keyup", () => {
+    spanFirstName.innerHTML = inputNames.value;
+})
+
+
+let inputAge = document.getElementById('age')
+let sectionHidden = document.getElementById('a-hard-truth')
+
+inputAge.addEventListener("input", () => {
+    (inputAge.value >= 18) ? (sectionHidden.style.visibility = "visible") : sectionHidden.style.visibility = "hidden";
+});
+
+let inputPwd = document.getElementById("pwd");
+let inputPwdconfirm = document.getElementById("pwd-confirm");
+
+
+inputPwd.addEventListener("input", () => {
+
+    (inputPwd.value.length < 6) ?
+        inputPwd.style.color = "red"
+        : inputPwd.style.color = "green"
+});
+
+inputPwdconfirm.addEventListener("input", () => {
+
+    (inputPwd.value == inputPwdconfirm.value) ?
+        inputPwdconfirm.style.color = "green"
+        : inputPwdconfirm.style.color = "red";
+});
+
+let toggleSelect = document.getElementById("toggle-darkmode");
+let body = document.querySelector("body");
+
+toggleSelect.addEventListener("change", () => {
+    console.log(toggleSelect.value)
+    switch (toggleSelect.value) {
+        case "dark":
+            body.style.backgroundColor = "black";
+            body.style.color = "white"
+            break
+        case "light":
+            body.style.backgroundColor = "white"
+            body.style.color = "black"
+            break
+    }
+}
+)
