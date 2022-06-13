985eb4afab21a10a9014a565c0b1329779f4743e
llyllyra <llyllyrallylly@gmail.com>
2022-06-13 12:04:55 +0200

    loop

--- 7.DOM-drill/script.js
+++ 7.DOM-drill/script.js
@@ -1,2 +1,21 @@
-let list = document.querySelector('ul')
+let list = document.querySelector("ul");
+let listMovie = list.childNodes;
+listMovie.forEach((element) => {
+    // fast and furious en premier
+    if (element.textContent === "Fast and Furious") {
+        element.classList.add("important");
+        let fastAndFurious = element;
+        list.replaceChild(fastAndFurious, list.firstChild)
+    }
 
+    // alert quand on clique 
+    element.addEventListener("click", () => {
+        if (element.classList == 'important') {
+            alert("The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family.")
+        }
+        else {
+            alert(element.textContent)
+        }
+    });
+
+});
